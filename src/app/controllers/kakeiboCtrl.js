7d93c2148683eaf9ef9be5c803e12610ced46d71
依田　剛 <takeshi@YodakeMBA.local>
2015-12-06 13:45:44 +0900

    graph追加

--- src/app/controllers/kakeiboCtrl.js
+++ src/app/controllers/kakeiboCtrl.js
@@ -56,31 +56,24 @@
 			var kakeibos = [];
 			kakeibos.push(new Kakeibo(kakeibos.length, new Date(), 1000, "お餅"));
 			kakeibos.push(new Kakeibo(kakeibos.length, new Date('2015/10/10'), 2000, "おかき"));
-			kakeibos.push(new Kakeibo(kakeibos.length, new Date('2015/10/12'), 2000, "おかき"));
+			kakeibos.push(new Kakeibo(kakeibos.length, new Date('2015/10/12'), 400, "おかき"));
 			return kakeibos;
 		};
 	}]);
 
-	// Graph描画サービス
-	app.service('GraphService', ['Kakeibo', 'SharedKakeiboService', function (Kakeibo, SharedKakeiboService) {
-		this.genDayOfCostGraphData = function () {
-			SharedKakeiboService.kakeibos;
-		};
-	}]);
 
 	// Module(Controller)
 	app.controller('KakeiboController', ['$scope', 'KakeiboService', 'Kakeibo', 'SharedKakeiboService', function ($scope, KakeiboService, Kakeibo, SharedKakeiboService) {
 
+			// watchでkakeibosを監視する。第三引数がtrueだと監視対象のオブジェクトのプロパティもウォッチする。
 			$scope.$watch(function () {
 				return $scope.kakeibos; //監視する値
 			}, function () {
 				SharedKakeiboService.kakeibos = $scope.kakeibos;
-			});
+			}, true);
 
 			// 一覧のデータバインド用変数
 			$scope.kakeibos = KakeiboService.getKakeibos();
-
-			//			SharedKakeiboService.kakeibos = $scope.kakeibos;
 
 			// 入力された家計簿
 			$scope.in_kakeibo = new Kakeibo(numberringId(), new Date(), 0, "");
@@ -89,6 +82,8 @@
 			$scope.add = function () {
 				var newone = angular.copy($scope.in_kakeibo);
 				newone.id = numberringId();
+				// 文字列としてバインドされてしまうのでIntに変換。本当は専用のディレクティブを作るのがいいらしい。
+				newone.money = parseInt(newone.money);
 				$scope.kakeibos.push(newone);
 			};
 
@@ -99,12 +94,14 @@
 					if (kakeibo.id === $scope.in_kakeibo.id) {
 						// spliceで配列の要素を置換
 						kakeibos.splice(idx, 1, angular.copy($scope.in_kakeibo));
+						// 文字列としてバインドされてしまうのでIntに変換。本当は専用のディレクティブを作るのがいいらしい。
+						kakeibos[idx].money = parseInt(kakeibos[idx].money);
 						kakeibos[idx].selected = true;
 					}
 				});
 			};
 
-			// 行削除
+			// 選択行の削除
 			$scope.delete = function () {
 				$scope.kakeibos = $scope.kakeibos.filter(function (kakeibo) {
 					return !kakeibo.selected;
@@ -134,36 +131,69 @@
 		}
 		]);
 
-	app.controller('GraphController', ['$scope', 'SharedKakeiboService', function ($scope, SharedKakeiboService) {
+	// Graph描画サービス
+	app.service('KakeiboGraphService', ['Kakeibo', function (Kakeibo) {
+		// 日毎の出費のグラフ用データ生成
+		// {"dates":["dates",....], "money":["金額", ...]}
+		this.convartToDayOfCostGraphData = function (kakeibos) {
+			// 日毎のお金集計
+			var dayOfCosts = [];
+			kakeibos.forEach(function (kakeibo) {
+				// 日付が一致するものを抽出
+				var gdata = _.findWhere(dayOfCosts, {
+					"date": kakeibo.formatDate()
+				});
+				if (gdata === undefined) {
+					// なければ追加
+					dayOfCosts.push({
+						"date": kakeibo.formatDate(),
+						"money": kakeibo.money
+					});
+				} else {
+					// あった場合には加算
+					gdata.money += kakeibo.money;
+				}
+			});
+			
+			// グラフデータ
+			var gdata = {"dates":[], "money":[]};
+			gdata.dates = dayOfCosts.map(function (d) { return d.date;});
+			gdata.money = dayOfCosts.map(function (d) { return d.money;});	
+			gdata.dates.unshift('dates');
+			gdata.money.unshift('金額');
+			return gdata;
+		};
+		}]);
+	
+	app.controller('GraphController', ['$scope', 'SharedKakeiboService', 'KakeiboGraphService', function ($scope, SharedKakeiboService, KakeiboGraphService) {
 		$scope.chart = null;
 		//watchを使った変更の監視
 		$scope.$watch(function () {
 			return SharedKakeiboService.kakeibos; //監視する値
 		}, function () {
-			var datas = SharedKakeiboService.kakeibos.map(function (kakeibo) {
-				return {
-					"money": kakeibo.money,
-					"date": kakeibo.formatDate
-				};
-			});
-
+			var datas = KakeiboGraphService.convartToDayOfCostGraphData(SharedKakeiboService.kakeibos);
 			$scope.chart.load({
-				json: datas,
-				keys: {
-					value: ['money'],
+					columns: [
+						datas.dates,
+						datas.money
+						]
 				}
-			});
-		});
+			);
+		}, true);
 
 		$scope.showGraph = function () {
+			// 初期化
 			$scope.chart = c3.generate({
 				bindto: '#chart',
 				data: {
-					json: [],
-					keys: {
-						value: ['money'],
-					},
+					x: 'dates',
+					columns: [],
 					type: 'bar'
+				},
+				axis: {
+					x: {
+						type: 'category'
+					}
 				}
 			});
 		};
