5e78ea4502c3a8309fd26528fd6ae38d4e3d9e25
依田　剛 <takeshi@YodakeMBA.local>
2016-01-16 17:44:49 +0900

    テストサンプル作成完了

--- src/app/controllers/kakeiboCtrl.spec.js
+++ src/app/controllers/kakeiboCtrl.spec.js
@@ -1,91 +1,97 @@
 /**
-* ユニットテストサンプル
-* 
-* 【Jasmineの機能】
-* describe: テストをグルーピングする宣言。ネストして書くことも可能
-* beforeEach : テスト実行前に実行する処理を書く Javaで言うとsetUp
-* it: ユニットテストを記述する。Javaで言うとこれがtestXxxxというテストメソッド。
-* 
-* 【ng-Mockの機能】
-* module: モジュールの読み込み
-* inject: ServiceやFactoryをDIする。パラメータの両端にアンダースコアをつけて宣言する。(つけなくてもできるけど)
-* 
-*/
+ * ユニットテストサンプル
+ * 
+ * 【Jasmineの機能】
+ * describe: テストをグルーピングする宣言。ネストして書くことも可能
+ * beforeEach : テスト実行前に実行する処理を書く Javaで言うとsetUp
+ * it: ユニットテストを記述する。Javaで言うとこれがtestXxxxというテストメソッド。
+ * 
+ * 【ng-Mockの機能】
+ * module: モジュールの読み込み
+ * inject: ServiceやFactoryをDIする。パラメータの両端にアンダースコアをつけて宣言する。(つけなくてもできるけど)
+ * 
+ */
 
-describe('factry: Kakeiboのテスト', function() {
-	// モジュールのインポート
-	beforeEach(module('frontWgApp'));
-	
-	// Kakeiboクラスを格納する変数
+describe('factry: Kakeiboのテスト', function () {
+	// Modelクラス格納用変数
 	var Kakeibo = null;
-	
-	// factoryからKakeiboクラス型を取得する
-	beforeEach(inject(function(_Kakeibo_) {
-		Kakeibo = _Kakeibo_;
-	}));
-	
-	describe('moneyDispのテスト', function() {
-		
-		it('メソッドが定義されていること', function() {
-			var kakeibo = new Kakeibo(new Date(), 100, 'おかき');
-			expect(kakeibo.moneyDisp()).toBeDefined();
-		});
-	
-		it('小数点以下2桁0埋めで金額がフォーマットされること', function() {
-			var kakeibo = new Kakeibo(new Date(), 100, 'おかき');
-			expect(kakeibo.moneyDisp()).toEqual('100.00');
-		});
-		
-		it('小数点以下2桁は四捨五入されること', function() {
-			// TODO 以下のテストが通るように元の処理を変更
-//			var kakeibo = new Kakeibo(new Date(), 100.255, 'おかき');
-//			expect(kakeibo.moneyDisp()).toEqual('100.26');			
-// 			kakeibo.money = 100.254;
-// 			expect(kakeibo.moneyDisp()).toEqual('100.25');
+
+	// 前処理
+	beforeEach(function () {
+		// アプリモジュールの読み込み
+		module('frontWgApp');
+
+		// Factory, Service定義を取得
+		inject(function (_Kakeibo_) {
+			Kakeibo = _Kakeibo_;
 		});
 	});
 
-	describe('formatDateのテスト', function() {
-		it('メソッドが定義されていること', function() {
+	describe('moneyDispのテスト', function () {
+
+		it('メソッドが定義されていること', function () {
+			var kakeibo = new Kakeibo(new Date(), 100, 'おかき');
+			expect(kakeibo.moneyDisp()).toBeDefined();
+		});
+
+		it('小数点以下2桁0埋めで金額がフォーマットされること', function () {
+			var kakeibo = new Kakeibo(new Date(), 100, 'おかき');
+			expect(kakeibo.moneyDisp()).toEqual('100.00');
+		});
+
+		it('小数点以下2桁は四捨五入されること', function () {
+			// TODO 以下のテストが通るように元の処理を変更
+			//			var kakeibo = new Kakeibo(new Date(), 100.255, 'おかき');
+			//			expect(kakeibo.moneyDisp()).toEqual('100.26');			
+			// 			kakeibo.money = 100.254;
+			// 			expect(kakeibo.moneyDisp()).toEqual('100.25');
+		});
+	});
+
+	describe('formatDateのテスト', function () {
+		it('メソッドが定義されていること', function () {
 			var kakeibo = new Kakeibo();
 			expect(kakeibo.formatDate()).toBeDefined();
 		});
-		
-		it('yyyy年mm月dd日で変換されること', function() {
+
+		it('yyyy年mm月dd日で変換されること', function () {
 			var kakeibo = new Kakeibo(new Date('2016/10/10'), 100, 'おかき');
 			expect(kakeibo.formatDate()).toEqual('2016年10月10日');
-		});	
-		
-		it('月日部分は0埋めで変換されること', function() {
+		});
+
+		it('月日部分は0埋めで変換されること', function () {
 			var kakeibo = new Kakeibo(new Date('2016/1/1'), 100, 'おかき');
 			expect(kakeibo.formatDate()).toEqual('2016年01月01日');
-		});	
+		});
 	});
 
 });
 
-describe('service: KakeiboServiceのテスト', function() {
-	// モジュールのインポート
-	beforeEach(module('frontWgApp'));
-	
-	// KakeiboServiceクラスを格納する変数
+describe('service: KakeiboServiceのテスト', function () {
+	// Service格納用変数
 	var KakeiboService = null;
-	var Kakeibo = null;	
-	
-	// factoryからKakeiboService, Kakeiboのクラス型を取得する
-	beforeEach(inject(function(_KakeiboService_, _Kakeibo_) {
-		KakeiboService = _KakeiboService_;
-		Kakeibo = _Kakeibo_;
-	}));
-	
-	
-	describe('getKakeibosのテスト', function() {
-		it ('Kakeiboのインスタンスが一覧で取得できること', function() {
+	// Modelクラス格納用変数
+	var Kakeibo = null;
+
+	// 前処理
+	beforeEach(function () {
+		// アプリモジュールの読み込み
+		module('frontWgApp');
+
+		// Factory, Service定義を取得
+		inject(function (_KakeiboService_, _Kakeibo_) {
+			KakeiboService = _KakeiboService_;
+			Kakeibo = _Kakeibo_;
+		});
+	});
+
+	describe('getKakeibosのテスト', function () {
+		it('Kakeiboのインスタンスが一覧で取得できること', function () {
 			// TODO: dateも検証したい。new Date()をモック化すれば検証できそう。
-			
+
 			var kakeibos = KakeiboService.getKakeibos();
 			expect(kakeibos.length).toEqual(2);
-			
+
 			// Kakeiboインスタンスであることの確認、値の検証を実施
 			expect(kakeibos[0]).toEqual(jasmine.any(Kakeibo));
 			expect(kakeibos[0].money).toEqual(1000);
@@ -99,48 +105,82 @@ describe('service: KakeiboServiceのテスト', function() {
 	});
 });
 
-describe('controller: KakeiboControllerのテスト', function() {
-	
-	// Appモジュールのインポート
-	beforeEach(module('frontWgApp'));
+describe('controller: KakeiboControllerのテスト', function () {
 
+	// controller格納用
 	var $controller;
+	// $scope格納用
 	var $scope;
-	var Kakeibo = null;	
+	// Modelクラス格納用変数
+	var Kakeibo = null;
+	// Service格納用変数
 	var KakeiboService = null;
 
-//	spyOn(meetingService, 'sendCancelNotices').andReturn(fakeHttpPromise);
-	beforeEach(function() {
+	beforeEach(function () {
+
+		module('frontWgApp');
+
 		$scope = {};
-		inject(function(_$controller_, _Kakeibo_, _KakeiboService_) {
+
+		// Serviceをモック化 (上書きするイメージ)
+		module(function ($provide) {
+			$provide.service('KakeiboService', ['Kakeibo', function (Kakeibo) {
+				// 'callFake'という記法をネットで見かけるがJasmine1系の記法なので以下を使う。
+				this.getKakeibos = jasmine.createSpy('getKakeibos').and.callFake(function () {
+					// KakeiboServiceもメソッドをモック化
+					var mockReturn = [];
+					mockReturn.push(new Kakeibo(new Date(), 100, 'testitem'));
+					return mockReturn;
+				});
+			}]);
+		});
+
+		// 定義をインジェクション
+		inject(function (_$controller_, _Kakeibo_, _KakeiboService_) {
 			$controller = _$controller_;
 			Kakeibo = _Kakeibo_;
 			KakeiboService = _KakeiboService_;
-			
-			// KakeiboServiceもメソッドをモック化
-			var mockReturn = [];
-			mockReturn.push(new Kakeibo(new Date(), 100, 'testitem'));
-			
-			spyOn(KakeiboService, 'getKakeibos').andReturn(mockReturn);
 		});
 	});
 
-	describe('コンストラクタのテスト', function() {
-		it('KakeiboService.getKakeibosがコールされること', function() {
-		
-			var controller = $controller('KakeiboController', {$scope: $scope, KakeiboService: KakeiboService, Kakeibo: Kakeibo});
-			// KakeiboService.getKakeibosがコールされていること
+	describe('コンストラクタのテスト', function () {
+		it('KakeiboService.getKakeibosがコールされること', function () {
+			$controller('KakeiboController', {
+				'$scope': $scope,
+				'KakeiboService': KakeiboService,
+				'Kakeibo': Kakeibo
+			});
+
+			// KakeiboService.getKakeibosがコールされること
 			expect(KakeiboService.getKakeibos).toHaveBeenCalled();
-			
+
 			// KakeiboService.getKakeibosのリプライが$Scopeに格納されていること
-			var kakeibos = $scope.kakeibos();
+			var kakeibos = $scope.kakeibos;
 			expect(kakeibos.length).toEqual(1);
 
 			// Kakeiboインスタンスであることの確認、値の検証を実施
 			expect(kakeibos[0]).toEqual(jasmine.any(Kakeibo));
 			expect(kakeibos[0].money).toEqual(100);
 			expect(kakeibos[0].item).toEqual('testitem');
-
 		});
+	});
+	
+	describe('家計簿一行削除のテスト', function () {
+		//		it('Scopeのテストを書く場合の例', function () {
+		//			$controller('KakeiboController', {
+		//				'$scope': $scope,
+		//				'KakeiboService': KakeiboService,
+		//				'Kakeibo': Kakeibo
+		//			});
+		//			
+		//			// 事前確認：kakeibosは1行分のはず
+		//			expect($scope.kakeibos.length).toEqual(1);
+		//
+		//			// メソッド実行：Scopeのメソッドをコール。
+		//			$scope.deleteKakeibos();
+		//			
+		//			// 検証：kakeibosが削除されているはず。
+		//			expect($scope.kakeibos.length).toEqual(0);
+		//		});
 	});
 });
